6e9f43241a7e5fbde2b8db25b0c589c186e759ae
Renêr <91918580+renercrisostomo@users.noreply.github.com>
2023-05-11 09:34:06 -0300

    adicionado validadores de input

--- ponto_flutuante.py
+++ ponto_flutuante.py
@@ -1,8 +1,3 @@
-base = int(input('Digite a base: '))
-digitos = int(input('Digite o número de dígitos: '))
-expoente = int(input('Digite o expoente: '))
-num = float(input('Digite um número: '))
-
 def ponto_flutuante(base, digitos, expoente, numero):
     if numero == 0:
         return 0
@@ -27,5 +22,43 @@ def ponto_flutuante(base, digitos, expoente, numero):
         numero = round(numero, digitos)
 
         return f"{sinal} {numero} * {base} ^ {expoenteCalculado}"
+
+base = -1
+while base <= 0:
+  try:
+    base = int(input('Digite a base: '))
+    if base <= 0:
+      raise ValueError('A base deve ser maior que zero.')
+  except ValueError as e:
+    print(f'Erro: {e}')
+    base = -1
+
+digitos = -1
+while digitos <= 0:
+  try:
+    digitos = int(input('Digite o número de dígitos: '))
+    if digitos <= 0:
+      raise ValueError('O número de dígitos deve ser maior que zero.')
+  except ValueError as e:
+    print(f'Erro: {e}')
+    digitos = -1
+
+expoente = -1
+while expoente <= 0:
+  try:
+    expoente = int(input('Digite o expoente de precisão: '))
+    if digitos <= 0:
+      raise ValueError('O expoente de precisão deve ser maior que zero.')
+  except ValueError as e:
+    print(f'Erro: {e}')
+    digitos = -1
+
+num = None
+while num == None:
+  try:
+    num = float(input('Digite um número: '))
+  except ValueError as e:
+    print(f'Erro: {e}')
+    num = None
     
 print(ponto_flutuante(base, digitos, expoente, num))
