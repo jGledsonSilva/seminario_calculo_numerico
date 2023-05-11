018433f6045784404028cc512797fd2ba1c3da38
Renêr <91918580+renercrisostomo@users.noreply.github.com>
2023-05-11 09:17:33 -0300

    modificado logica

--- ponto_flutuante.py
+++ ponto_flutuante.py
@@ -1,35 +1,32 @@
 
 base = int(input('Digite a base: '))
 digitos = int(input('Digite o número de dígitos: '))
-menorExpoente = int(input('Digite o menor expoente: '))
-maiorExpoente = int(input('Digite o maior expoente: '))
+expoente = int(input('Digite o expoente: '))
 num = float(input('Digite um número: '))
 
-def ponto_flutuante(base, digitos, menorExpoente, maiorExpoente, num):
-    if num == 0:
+def ponto_flutuante(base, digitos, expoente, numero):
+    if numero == 0:
         return 0
     else:
         sinal = "+"
-        if num < 0:
+        if numero < 0:
             sinal = "-"
-            num = -num
-        expoente = 0
+            numero = -numero
 
-        if num >= base or num < 1:
-            while num >= base:
-                num /= base
-                expoente += 1
-            while num < 1:
-                num *= base
-                expoente -= 1
-            num /= base
-            expoente += 1
+        expoenteCalculado = 0
+        while numero >= base:
+            numero /= base
+            expoenteCalculado += 1
+        while numero < 0.1:
+            numero *= base
+            expoenteCalculado -= 1
 
-        if expoente < menorExpoente:
-            expoente = menorExpoente
-        elif expoente > maiorExpoente:
-            expoente = maiorExpoente
-        num = round(num, digitos)
-        return f"{sinal} {num} * {base} ^ {expoente}"
+        if expoenteCalculado < -expoente:
+            expoenteCalculado = -expoente
+        elif expoenteCalculado > expoente:
+            expoenteCalculado = expoente
+        numero = round(numero, digitos)
+
+        return f"{sinal} {numero} * {base} ^ {expoenteCalculado}"
     
-print(ponto_flutuante(base, digitos, menorExpoente, maiorExpoente, num))
+print(ponto_flutuante(base, digitos, expoente, num))
